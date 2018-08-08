10b225b091483632981837534f95216fb21a343a
ilya <rinekri@gmail.com>
2018-08-08 22:24:37 +0200

    Init commit

---
+++ app/src/main/kotlin/ru/rinekri/devfest2018/classic/DucksClassicActivity.kt
@@ -0,0 +1,13 @@
+package ru.rinekri.devfest2018.classic
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import ru.rinekri.devfest2018.R
+
+class DucksClassicActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_ducks_classsic)
+  }
+}
---
+++ app/src/main/kotlin/ru/rinekri/devfest2018/delegates/DucksDelegatesActivity.kt
@@ -0,0 +1,13 @@
+package ru.rinekri.devfest2018.delegates
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import ru.rinekri.devfest2018.R
+
+class DucksDelegatesActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_ducks_classsic)
+  }
+}
