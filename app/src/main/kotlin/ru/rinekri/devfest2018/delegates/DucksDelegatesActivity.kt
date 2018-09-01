46878c8ee50b3897358411115bfe78a5548466a9
Ilya Nekrasov <ni@appkode.ru>
2018-09-01 17:50:30 +0200

    [chapter 2, stage 2] Reorganize activity and add logic to update all data on header click

--- app/src/main/kotlin/ru/rinekri/devfest2018/delegates/DucksDelegatesActivity.kt
+++ app/src/main/kotlin/ru/rinekri/devfest2018/delegates/DucksDelegatesActivity.kt
@@ -12,54 +12,64 @@ import ru.rinekri.devfest2018.items.common.DisplayableItem
 
 class DucksDelegatesActivity : AppCompatActivity() {
 
+  private val advert = DuckMockData.adverts.orEmpty().map { AdvertItem(it.icon, it.tagline) }.shuffled().first()
+
+  private lateinit var ducksList: RecyclerView
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_ducks_delegates)
-    findViewById<RecyclerView>(R.id.duckList).apply {
+    ducksList = findViewById(R.id.duckList)
+
+    ducksList.apply {
       layoutManager = LinearLayoutManager(this@DucksDelegatesActivity)
-      adapter = DucksDelegatesAdapter(
-        onDuckCountClickAction = {
-          AlertDialog
-            .Builder(this@DucksDelegatesActivity)
-            .setTitle("Спасибо!")
-            .setMessage("Ваша заявка на уточку в количестве ${it.count} штук принята и ожидает обработки.")
-            .setPositiveButton("Прекрасно", null)
-            .show()
-        },
-        onAdvertClickAction = {
-          AlertDialog
-            .Builder(this@DucksDelegatesActivity)
-            .setTitle("Готово!")
-            .setMessage("Ваш заказ на соль успешно оформлен, мы с вами свяжемся.")
-            .setPositiveButton("ОК", null)
-            .show()
-        },
-        onSlipperClickAction = {
-          AlertDialog
-            .Builder(this@DucksDelegatesActivity)
-            .setTitle("Спасибо!")
-            .setMessage("Ваша заявка на шлепки с уточками принята и ожидает обработки.")
-            .setPositiveButton("Хорошо", null)
-            .show()
-        },
-        onHeaderClickAction = {
-          // TODO: Логика скрытия / раскрытия групп
-        }
-      ).apply {
-        val data = mutableListOf<DisplayableItem>().apply {
-          add(getAdvert())
-          add(HeaderItem(false, R.string.rubber_ducks))
-          addAll(getRubberDucks())
-          add(HeaderItem(false, R.string.slippers))
-          addAll(getDuckSlippers())
-        }
-        setData(data)
-      }
+      adapter = createAdapter().apply { showData() }
     }
   }
 
-  private fun getAdvert(): DisplayableItem {
-    return DuckMockData.adverts.orEmpty().map { AdvertItem(it.icon, it.tagline) }.shuffled().first()
+  fun createAdapter(): DucksDelegatesAdapter {
+    val ducksDelegatesAdapter = DucksDelegatesAdapter(
+      onDuckCountClickAction = {
+        AlertDialog
+          .Builder(this@DucksDelegatesActivity)
+          .setTitle("Спасибо!")
+          .setMessage("Ваша заявка на уточку в количестве ${it.count} штук принята и ожидает обработки.")
+          .setPositiveButton("Прекрасно", null)
+          .show()
+      },
+      onAdvertClickAction = {
+        AlertDialog
+          .Builder(this@DucksDelegatesActivity)
+          .setTitle("Готово!")
+          .setMessage("Ваш заказ на соль успешно оформлен, мы с вами свяжемся.")
+          .setPositiveButton("ОК", null)
+          .show()
+      },
+      onSlipperClickAction = {
+        AlertDialog
+          .Builder(this@DucksDelegatesActivity)
+          .setTitle("Спасибо!")
+          .setMessage("Ваша заявка на шлепки с уточками принята и ожидает обработки.")
+          .setPositiveButton("Хорошо", null)
+          .show()
+      },
+      onHeaderClickAction = {
+//        TODO: Добавить логику скрытия/раскрытия элементов
+        (ducksList.adapter as DucksDelegatesAdapter).apply { showData() }
+      }
+    )
+    return ducksDelegatesAdapter
+  }
+
+  private fun DucksDelegatesAdapter.showData() {
+    val data = mutableListOf<DisplayableItem>().apply {
+      add(advert)
+      add(HeaderItem(false, R.string.rubber_ducks))
+      addAll(getRubberDucks())
+      add(HeaderItem(false, R.string.slippers))
+      addAll(getDuckSlippers())
+    }
+    setData(data)
   }
 
   private fun getRubberDucks(): List<DisplayableItem> {
